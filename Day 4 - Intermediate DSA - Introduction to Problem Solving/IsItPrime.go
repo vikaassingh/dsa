8bb71c3b0d4096122596e7cf9723120cbbdc6c61
Vikas Singh <vikas26singh11@gmail.com>
2025-03-09 13:37:30 +0530

    IsItPrime

---
+++ Day 4 - Intermediate DSA - Introduction to Problem Solving/IsItPrime.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A int
+	fmt.Scanf("%d", &A)
+	flag := true
+	for i := 2; i < A/2; i++ {
+		if A%i == 0 {
+			flag = false
+			break
+		}
+	}
+	if flag {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+
+}
